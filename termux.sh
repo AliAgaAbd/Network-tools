50642f754557304f471951a4427ff74d498ef887
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-02 20:20:07 +0330

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 pkg update && pkg upgrade
-sudo apt update
 pkg install wget
 pkg install p7zip
-pkg install wget p7zip
 wget https://raw.githubusercontent.com/AliAgaAbd/Network-tools/refs/heads/main/linux-x64.7z -O myproject.7z
-7z x myproject.7z -O myproject
+7z x myproject.7z -omyproject
 cd myproject
 ./Program
