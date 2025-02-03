dc93c068bc585e8d2450f1501adc786d6a4e6294
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-03 09:12:01 +0330

    Fix termux.sh

--- termux.sh
+++ termux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-pkg update && pkg upgrade
-pkg install wget
-pkg install p7zip
-wget https://raw.githubusercontent.com/AliAgaAbd/Network-tools/refs/heads/main/linux-x64.7z -O myproject.7z
-7z x myproject.7z -o myproject
-cd myproject
-.\Program
+pkg install wget -y
+wget https://raw.githubusercontent.com/AliAgaAbd/Network-tools/refs/heads/main/linux-x64.7z -O /tmp/linux-x64.7z
+pkg install p7zip -y
+7z x /tmp/linux-x64.7z -o/tmp/
+cd /tmp/linux-x64
+./Program
