4ba72ee516d9af634176780a994fa1610c333ea1
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-04 14:45:34 +0330

    Fix termux.sh

--- termux.sh
+++ termux.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
-
-# بررسی و نصب curl در صورت عدم نصب
 if ! command -v curl &> /dev/null; then
-    echo "curl نصب نشده است. در حال نصب..."
+    echo "curl is not installed. Installing..."
     sudo pacman -S curl --noconfirm
 fi
-
-# بررسی و نصب .NET SDK در صورت عدم نصب
 if ! command -v dotnet &> /dev/null; then
-    echo ".NET SDK نصب نشده است. در حال نصب..."
+    echo ".NET SDK is not installed. Installing..."
     sudo pacman -S dotnet-sdk --noconfirm
 fi
-
-# ایجاد پروژه جدید C# اگر پروژه‌ای وجود ندارد
 if [ ! -d "my_project" ]; then
     dotnet new console -o my_project --force
 fi
-
-# انتقال فایل C# به پروژه
 mv Program.cs my_project/
-
-# رفتن به دایرکتوری پروژه
 cd my_project || exit
-
-# ساخت و اجرای برنامه C#
 dotnet run
+echo "Started successfully!"
