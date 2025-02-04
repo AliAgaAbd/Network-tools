988e0408987c75a4aa7f43e666d73be1f5e6e9a4
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-04 15:02:35 +0330

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-if ! command -v curl &> /dev/null; then
-    echo "curl is not installed. Installing..."
-    sudo pacman -S curl --noconfirm
-fi
-if ! command -v dotnet &> /dev/null; then
-    echo ".NET SDK is not installed. Installing..."
-    sudo pacman -S dotnet-sdk --noconfirm
-fi
-if [ ! -d "my_project" ]; then
-    dotnet new console -o my_project --force
-fi
-mv Program.cs my_project/
-cd my_project || exit
-dotnet run
-echo "Started successfully!"
+install_git() {
+    if ! command -v git &> /dev/null; then
+        echo "git is not installed. Installing..."
+        sudo pacman -S git --noconfirm
+    fi
+}
+install_dotnet() {
+    if ! command -v dotnet &> /dev/null; then
+        echo ".NET SDK is not installed. Installing..."
+        sudo pacman -S dotnet-sdk --noconfirm
+    fi
+}
+clone_project() {
+    echo "Cloning project from GitHub..."
+    git clone https://github.com/AliAgaAbd/Network-tools.git
+}
+run_dotnet_project() {
+    cd Network-tools || exit
+    dotnet restore
+    dotnet run
+}
+print_success() {
+    echo "Project started successfully!"
+}
+install_git
+install_dotnet
+clone_project
+run_dotnet_project
+print_success
