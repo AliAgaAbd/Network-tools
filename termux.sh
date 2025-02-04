13e5bba081207c6e383c66ec22367038e59b6880
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-04 14:28:34 +0330

    termux.sh

--- termux.sh
+++ termux.sh
@@ -1,20 +1,17 @@
-#!/data/data/com.termux/files/usr/bin/sh
-pkg update -y
-pkg install -y wget curl
-if ! command -v dotnet &> /dev/null; then
-    echo "dotnet not installed! Installing dotnet 8.0..."
-    wget https://download.visualstudio.microsoft.com/download/pr/d35f2de9-2d13-47c9-aeae-d0834a3d40f2/122d0e570da870e52cd60784d9e24be7/dotnet-sdk-8.0.100-linux-x64.tar.gz -O dotnet.tar.gz
-    mkdir -p $HOME/dotnet
-    tar -xzvf dotnet.tar.gz -C $HOME/dotnet
-    export PATH=$PATH:$HOME/dotnet
-    
-    echo "dotnet 8.0 installed successfully!"
-else
-    echo "dotnet is already installed!"
+#!/bin/bash
+if ! command -v curl &> /dev/null; then
+    echo "curl نصب نشده است. در حال نصب..."
+    sudo pacman -S curl --noconfirm
 fi
-mkdir -p ~/cs_project && cd ~/cs_project || exit
-curl -L -o Program.cs "https://raw.githubusercontent.com/AliAgaAbd/Network-tools/main/Program.cs"
-dotnet new console -o my_project --force
+if ! command -v dotnet &> /dev/null; then
+    echo ".NET SDK نصب نشده است. در حال نصب..."
+    sudo pacman -S dotnet-sdk --noconfirm
+fi
+if [ ! -d "my_project" ]; then
+    dotnet new console -o my_project --force
+fi
 mv Program.cs my_project/
 cd my_project || exit
 dotnet run
+chmod +x termux.sh
+./termux.sh
