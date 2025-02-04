0bfc4c62a957fd4093939ea9bd9577f54fadaa18
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-04 13:22:06 +0330

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,8 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/sh
-tpkg update && pkg install -y dotnet
-mkdir -p ~/cs_project && cd ~/cs_project
-curl -o Program.cs https://raw.githubusercontent.com/AliAgaAbd/Network-tools/refs/heads/main/Program.cs
+pkg update -y && pkg install -y dotnet
+mkdir -p ~/cs_project && cd ~/cs_project || exit
+curl -L -o Program.cs "https://raw.githubusercontent.com/AliAgaAbd/Network-tools/main/Program.cs"
+if ! command -v dotnet &> /dev/null; then
+    echo "dotnet not installed!"
+    exit 1
+fi
 dotnet new console -o my_project --force
 mv Program.cs my_project/
-cd my_project
+cd my_project || exit
 dotnet run
