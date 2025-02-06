449d1517e91570cc4d1e21e4f3cc960cd0288e36
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-06 15:53:14 +0330

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,13 +1,13 @@
 pkg upgrade
-pkg install proot-distro -y
-proot-distro install ubuntu -y
-proot-distro login ubuntu -y
-apt update && apt upgrade -y
-apt dist-upgrade -y
-apt install dotnet-sdk-8.0 -y
-apt install dotnet-runtime-8.0 -y
-apt install wget -y
-apt install curl -y
+pkg install proot-distro
+proot-distro install ubuntu
+proot-distro login ubuntu
+apt update && apt upgrade
+apt dist-upgrade
+apt install dotnet-sdk-8.0
+apt install dotnet-runtime-8.0
+apt install wget
+apt install curl
 mkdir -p my_project
 wget -O my_project/Program.cs "https://raw.githubusercontent.com/AliAgaAbd/Network-tools/main/Program.cs"
 cd my_project
