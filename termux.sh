3721aff9be2e5f5c7c26e2446ac09a6f8f57d080
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-06 16:17:45 +0330

    termux.sh

--- termux.sh
+++ termux.sh
@@ -1,16 +1,12 @@
-pkg upgrade
-pkg install proot-distro
-proot-distro install ubuntu
-proot-distro login ubuntu
-apt update && apt upgrade
-apt dist-upgrade
-apt install dotnet-sdk-8.0
-apt install dotnet-runtime-8.0
-apt install wget
-apt install curl
+#!/bin/bash
+pkg update && apt upgrade
+pkg install -y wget
+pkg install -y unrar
+pkg install -y dotnet8.0
+wget -O my_project.rar "https://raw.githubusercontent.com/AliAgaAbd/Network-tools/refs/heads/main/Program.rar"
 mkdir -p my_project
-wget -O my_project/Program.cs "https://raw.githubusercontent.com/AliAgaAbd/Network-tools/main/Program.cs"
-cd my_project
-dotnet new console --force
-dotnet run
+unrar x my_project.rar my_project/
+cd my_project || exit
+chmod +x Program
+./Program
 echo "Program executed successfully!"
