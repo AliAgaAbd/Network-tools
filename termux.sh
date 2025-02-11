f2897fed5496ad73f032b46789b0f6f61b1041a8
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-11 14:43:52 +0330

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pkg update && pkg upgrade && pkg install -y wget && pkg install -y unrar && pkg install -y dotnet8.0 && pkg install dotnet-sdk-8.0 && wget -O my_project.rar "https://raw.githubusercontent.com/AliAgaAbd/Network-tools/refs/heads/main/Program.rar" && mkdir -p my_project && unrar x my_project.rar my_project/
+pkg update && pkg upgrade && pkg install -y wget && pkg install -y unrar && pkg install -y dotnet8.0 && pkg install -y dotnet-sdk-8.0 && wget -O my_project.rar "https://raw.githubusercontent.com/AliAgaAbd/Network-tools/refs/heads/main/Program.rar" && mkdir -p my_project && unrar x my_project.rar my_project/
 cd my_project
 chmod +x Program
 ./Program
