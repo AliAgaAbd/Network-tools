fb693522f24980865794200c274e57bfb74ee1ff
Ali.aga <168641407+AliAgaAbd@users.noreply.github.com>
2025-02-12 14:33:26 +0330

    Delete termux.sh

--- termux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pkg update && pkg upgrade && pkg install -y wget && pkg install -y unrar && pkg install -y dotnet8.0 && pkg install -y dotnet-sdk-8.0 && wget -O Program.rar "https://raw.githubusercontent.com/AliAgaAbd/Network-tools/refs/heads/main/Program.rar" && mkdir -p Program && unrar x Program.rar Program/
-cd Program
-chmod +x Program
-./Program
